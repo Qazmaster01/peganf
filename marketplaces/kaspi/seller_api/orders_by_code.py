d82a5aed51e201c3758473f95372b731b9493942
Kaden Aset <kadenaset2@gmail.com>
2024-03-13 23:26:43 +0600

    feat: Добавлен валидация

--- marketplaces/kaspi/seller_api/orders_by_code.py
+++ marketplaces/kaspi/seller_api/orders_by_code.py
@@ -4,11 +4,32 @@ from rest_framework.views import APIView
 from apps.api_key import services
 from utils.kaspi import get_kaspi_http_headers
 
+
 class KaspiOrdersByCodeAPI(APIView):
+    """Получение состава заказа по коду."""
+
     def get(self, request):
-        token = services.get_kaspi_token_by_user(request.user)
-        url = f"https://kaspi.kz/shop/api/v2/orders?filter[orders][code]={request.data.get('code')}"
-        response = requests.get(url=url, headers=get_kaspi_http_headers(token))
-        return Response(response.json() if "data" in response.json() else {"error": "Неправильный запрос на каспи", "data": response.content}, status=201 if "data" in response.json() else 500)
+        try:
+            if not request.user:
+                return Response({"error": "Пользователь не найдено"}, status=400)
+
+            token = services.get_kaspi_token_by_user(request.user)
+            codes = request.data.get('code')
+            if not codes:
+                return Response({"error": "Код заказа не найдено"}, status=400)
+
+            url = f"https://kaspi.kz/shop/api/v2/orders?filter[orders][code]={codes}"
+            r = requests.get(url=url, headers=get_kaspi_http_headers(token))
+            if r.status_code == 200:
+                data = r.json().get("data")
+                if data:
+                    return Response(data, status=200)
+                else:
+                    return Response({"error": "Нет данных в ответе от Каспий"}, status=500)
+            else:
+                return Response({"error": "Ошибка при запросе к Каспий", "data": r.content}, status=r.status_code)
+
+        except Exception as e:
+            return Response({"error": str(e)}, status=500)
 
 
